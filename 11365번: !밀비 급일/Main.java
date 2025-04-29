c444818f1321f0664d223cca4ae9f1c415ab297d
noiskk <zionzion00@naver.com>
2025-04-29 16:32:20 +0900

    BOJ-EX

---
+++ 11365번: !밀비 급일/Main.java
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 11365                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: zionzion00 <boj.kr/u/zionzion00>            +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/11365                          #+#        #+#      #+#    */
+/*   Solved: 2025/07/04 17:01:48 by zionzion00    ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb;
+
+    String s = br.readLine();
+    while(!s.equals("END")){
+      sb = new StringBuilder();
+      sb.append(s).reverse();
+      System.out.println(sb);
+      s = br.readLine();
+    }
+
+    
+  }
+}
