15eb94fdf3a98d1c89ef5a1c0d638676474e632a
noiskk <zionzion00@naver.com>
2025-07-20 16:32:20 +0900

    Better Solution

--- Silver/2075번: N번째 큰 수/Main.java
+++ Silver/2075번: N번째 큰 수/Main.java
@@ -15,25 +15,50 @@ import java.util.*;
 
 public class Main{
   public static void main(String[] args) throws IOException{
+    
+    /*
+     * 1 <= N <= 1500
+     * 시간 제한 1초 : 100,000,000
+     * N^2 가능, N^3 불가능
+     */
+
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-
     int N = Integer.parseInt(br.readLine());
+    
 
-    Queue<Integer> pQueue = new PriorityQueue<>();
+    int[][] board = new int[N][N];
+    for(int i = 0; i < N; i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
 
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
       for (int j = 0; j < N; j++) {
-        int input = Integer.parseInt(st.nextToken());
-        pQueue.offer(input);
-        if(pQueue.size() > N){
-          pQueue.poll();
-        }
+        board[i][j] = Integer.parseInt(st.nextToken());
       }
     }
+    
+    // 최대 힙: b[0] - a[0] -> 내림차순 정렬
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
 
-    System.out.println(pQueue.peek());
+    // 맨 아랫줄을 [값, 행, 열] 형태로 저장
+    for (int j = 0; j < N; j++) {
+      pq.add(new int[]{board[N - 1][j], N - 1, j});
+    }
 
+    // pq 는 항상 사이즈 5 유지
+
+    // 한 칸 위의 값도 하나씩 확인
+    for (int i = 0; i < N - 1; i++) {
+      int[] current = pq.poll(); // 현재 가장 큰 수
+      int row = current[1];
+      int col = current[2];
+
+      // 해당 수의 윗 칸을 큐 추가 (그 다음으로 큰 수)
+      if (row > 0) {
+          pq.add(new int[]{board[row - 1][col], row - 1, col});
+      }
+    }
+  
+    System.out.println(pq.peek()[0]);
+
+  
   }
 }
