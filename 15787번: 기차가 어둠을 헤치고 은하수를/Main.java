0a0f31508050c74fab459466bce5f86e121b6c41
noiskk <zionzion00@naver.com>
2025-07-22 16:32:20 +0900

    Better Solution

--- 15787번: 기차가 어둠을 헤치고 은하수를/Main.java
+++ 15787번: 기차가 어둠을 헤치고 은하수를/Main.java
@@ -1,80 +1,60 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                      :::    :::    :::     */
-/*   Problem Number: 15787                             :+:    :+:      :+:    */
-/*                                                    +:+    +:+        +:+   */
-/*   By: zionzion00 <boj.kr/u/zionzion00>            +#+    +#+          +#+  */
-/*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/15787                          #+#        #+#      #+#    */
-/*   Solved: 2025/07/21 12:25:57 by zionzion00    ###          ###   ##.kr    */
-/*                                                                            */
-/* ************************************************************************** */
-
 import java.io.*;
 import java.util.*;
 
-
 public class Main{
+  static boolean[][] trains;
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
+    int N = Integer.parseInt(st.nextToken()); // 기차 수
+    trains = new boolean[N][20]; // 기차 좌석
+    int M = Integer.parseInt(st.nextToken()); // 명령 수
 
-    boolean[][] passengers = new boolean[N][20];
-
-    Set<String> set = new HashSet<>();
-
+    // 명령어 수행
     for (int i = 0; i < M; i++) {
       st = new StringTokenizer(br.readLine());
+      int command = Integer.parseInt(st.nextToken());
+      int train = Integer.parseInt(st.nextToken()) - 1;
 
-      switch (Integer.parseInt(st.nextToken())) {
-          case 1 :
-              int train = Integer.parseInt(st.nextToken()) - 1;
-              int seat = Integer.parseInt(st.nextToken()) - 1;
-              passengers[train][seat] = true;
-              break;
-          case 2:
-              train = Integer.parseInt(st.nextToken()) - 1;
-              seat = Integer.parseInt(st.nextToken()) - 1;
-              passengers[train][seat] = false;
-              break;
-          case 3:
-              train = Integer.parseInt(st.nextToken()) - 1;
-              for(int j = 19; j > 0; j--){
-                passengers[train][j] = passengers[train][j - 1];
-              }
-              passengers[train][0] = false;
-              break;
-          case 4:
-              train = Integer.parseInt(st.nextToken()) - 1;
-              for(int j = 0; j < 19; j++){
-                passengers[train][j] = passengers[train][j + 1];
-              }
-              passengers[train][19] = false;
-              break;
+      switch (command) {
+        case 1: // 승차
+          int seat = Integer.parseInt(st.nextToken()) - 1;
+          trains[train][seat] = true;
+          break;
+        case 2: // 하차
+          seat = Integer.parseInt(st.nextToken());
+          trains[train][seat] = false;
+          break;
+        case 3:
+          // 한칸씩 뒤로 밀기
+          for (int j = 19; j > 0; j--) {
+            trains[train][j] = trains[train][j - 1];
+          }
+          trains[train][0] = false;
+          break;
+        case 4:
+          // 한칸식 앞으로 밀기
+          for (int j = 0; j < 19; j++) {
+            trains[train][j] = trains[train][j + 1];
+          }
+          trains[train][19] = false;
+          break;
       }
+    
     }
 
+    // 건널 수 있는 기차 확인
+    Set<String> set = new HashSet<>();
     for (int i = 0; i < N; i++) {
       StringBuilder sb = new StringBuilder();
       for (int j = 0; j < 20; j++) {
-        sb.append(passengers[i][j] ? "1" : "0");
+
+        sb.append(trains[i][j] ? "1" : "0");
       }
       set.add(sb.toString());
     }
-
-
     System.out.println(set.size());
-
   }
 
-  public static List<Boolean> toList(boolean[] arr){
-    List<Boolean> list = new ArrayList<>();
-    for(boolean b : arr){
-      list.add(b);
-    }
-    return list;
-  }
 }
